6b98a97b9403bf570807df123f47ecc1f590b162
CodeSoul Solutions <codesoulsolutionscontent@gmail.com>
2025-04-25 14:34:03 +0530

    Add files via upload

---
+++ src/app/user-form/user-form.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  imports: [CommonModule,ReactiveFormsModule],
+  templateUrl: './user-form.component.html',
+  styleUrl: './user-form.component.css'
+})
+export class UserFormComponent {
+
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('Form Submitted', this.userForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
